0ee04232d06eb25e72f215e408b7828872986fdd
unknown <chris.schertenlieb@owlcomputing.com>
2018-04-14 12:05:31 -0400

    instructor portal

--- res_instructor/instructor.php
+++ res_instructor/instructor.php
@@ -2,10 +2,10 @@
     session_start();
     
     // Get all constants from config file.
-    include '../config.php';
-
+    include_once '../config.php';
     // Authenticate that the user came from BlackBoard.
     require_once($path_auth);
+
 
     $conn = mysqli_connect("34.224.83.184","student33","phppass33","student33");
     if(isset($_POST['submit_btn'])) {
@@ -15,7 +15,7 @@
 	$description = mysqli_real_escape_string($conn, $_POST['description']);
 	/* $instructor */
 	$course_id = $_SESSION['course_id'];
-    $user_name = $_SESSION['user_name'];
+        $user_name = $_SESSION['user_name'];
 
 	
 
@@ -27,9 +27,13 @@ $query = <<<SQL
 SQL;
 
 	$result = mysqli_query($conn, $query); 
-
+	if($result=true){
+	    unset($_POST['submit_btn']);
+	}
     }
 ?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -38,128 +42,27 @@ SQL;
     <link href="https://code.jquery.com/ui/jquery-ui-git.css" rel="stylesheet" type="text/css" />
     <script src="https://code.jquery.com/jquery-git.js"></script>
     <script src="https://code.jquery.com/ui/jquery-ui-git.js"></script>
-<style>
-* {
-    box-sizing: border-box;
-}
-
-
-input[type=text], select, textarea {
-    width: 100%;
-    padding: 12px;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    resize: vertical;
-}
-
-label {
-    padding: 12px 12px 12px 0;
-    display: inline-block;
-}
-
-input[type=submit] {
-    background-color: #0065a4;
-    color: white;
-    padding: 12px 20px;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-    float: right;
-    margin-top: 10px;
-}
-
-input[type=submit]:hover {
-    color: black;
-}
-.page {
-    background-color: #0065a4;
-}
-
-.header {
-    width: 80%;
-    margin: auto;
-    background-color: white;
-    padding: 10px 10px;
-    text-align: center;
-}
-
-.container {
-    border-radius: 5px;
-    background-color: #f2f2f2;
-    padding: 20px;
-    width: 90%;
-    margin: auto;
-}
-
-.col-25 {
-    float: left;
-    width: 25%;
-    margin-top: 6px;
-}
-
-.col-75 {
-    float: left;
-    width: 75%;
-    margin-top: 6px;
-}
-
-.post {
-    background-color: white;
-    width: 30%;
-    padding: 15px;
-    border: 1px solid;
-    box-shadow: 0 4px 10px 0 rgba(0,0,0,0.2), 0 4px 20px 0 rgba(0,0,0,0.19);
-}
-
-.stats_btn {
-    margin: 20px auto;
-    width: 50%;
-}
-#btn {
-    background-color: white;
-    color: black;
-    padding: 12px 20px;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-    display: block;
-    margin: auto;
-}
-
-.stats {
-    background-color: white;
-    width: 70%;
-    margin: auto;     
-}
-
-/* Clear floats after the columns */
-.row:after {
-    content: "";
-    display: table;
-    clear: both;
-}
-
-/* Responsive layout - when the screen is less than 600px wide, make the two columns stack on top of each other instead of next to each other */
-@media screen and (max-width: 600px) {
-    .col-25, .col-75, input[type=submit] {
-        width: 100%;
-        margin-top: 0;
-    }
-}
-</style>
-
+    <link rel="stylesheet" href="../res_student/css/css_main.css">
+    <script type="text/javascript" src="../res_student/js/js_config.js"></script>
+    <script type="text/javascript" src="../res_student/js/js_util.js"></script>
+    <link rel="stylesheet" type="text/css" href="./css/instructor_styles.css">
 </head>
 
 
 <body>
 <div class="page">
 <br/><br/>
+
+<!-- HEADER -->
 <div class="header">
 <h1>$teamName</h1>
-<h2>Share a Resource with [get course name]</h2>
-<p>Resize the browser window to see the effect. When the screen is less than 600px wide, make the two columns stack on top of each other instead of next to each other.</p>
+<h2>Share a Resource with <?php echo "courseName" ?></h2>
 </div>
 <br /><br />
+
+<!-- CONTENT -->
+
+<!-- FORM -->
 <div class="container">
   <form action="#" method="POST">
     <div class="row">
@@ -205,7 +108,9 @@ input[type=submit]:hover {
     </div>
   </form>
 </div>
+<!-- END FORM -->
 
+<!-- SHOW POSTS -->
 <div id="posts-container">
 <br/>
 <h3>Posts</h3>
@@ -223,21 +128,28 @@ input[type=submit]:hover {
 		echo $row['link'];
 		echo $row['description'];
 	    echo "</div>";
-	    echo "<br/><br/>";
-	}	
-    ?>     
+}	    echo "<br/><br/>";
+?>     
 <br/><br/>
 </div>
+<!-- END POSTS -->
 
+<!-- STATS BUTTON -->
 <div class="stats_btn">
     <button id="btn" type="button">Get Resource Statistics for this Course</button>
 </div>
 
+<!-- STATS DIV, CANVASES -->
 <div class="stats">
 
     <div id="canvas-holder" style="width:40%;margin: auto;">
 	<h4 style="text-align: center">Each post type as a percentage of all posts</h4>
-        <canvas id="chart-area">
+        <canvas id="chart-area-1"></canvas>
+    </div>
+
+    <div id="canvas-holder" style="width:40%;margin: auto;">
+	<h4 style="text-align: center">Number of upvotes per post type</h4>
+        <canvas id="chart-area-2"></canvas>
     </div>
 
 <?PHP
@@ -263,22 +175,23 @@ input[type=submit]:hover {
 <script>
     $(".stats").hide();
     $("#btn").click(function(){
-         $(".stats").show();
+	$(".stats").show();
+        
     });
 </script>
 
-<script>
+<!--<script>
 var randomScalingFactor = function() {
         return Math.round(Math.random() * 100);
 };
 
-var config = {
+var config-1 = {
         type: 'pie',
         data: {
                 datasets: [{
                     data: [
                 <?PHP
-                echo $numWebsites.",".$numBooks.",".$numArticles.",".$numVideos;
+                /*echo $numWebsites.",".$numBooks.",".$numArticles.",".$numVideos*/;
                 ?>
                 ],
                 backgroundColor: [
@@ -301,7 +214,7 @@ var config = {
         }
     };
 window.onload = function() {
-        var ctx = document.getElementById("chart-area").getContext("2d");
+        var ctx = document.getElementById("chart-area-1").getContext("2d");
         window.myPie = new Chart(ctx, config);
     };
 
@@ -338,7 +251,155 @@ window.onload = function() {
         config.data.datasets.splice(0, 1);
         window.myPie.update();
     });
-    </script>
+</script> -->
+
+<?PHP
+
+	$numWebsiteVotes = 0;
+	$numBookVotes = 0;
+	$numArticleVotes = 0;
+	$numVideoVotes = 0;
+	$tempId;
+
+	$result = mysqli_query($conn, $websiteQuery);
+	while($row = mysqli_fetch_array($result)){
+	    $tempId = $row['post_id'];
+            $countQuery = 'SELECT * FROM project_upvotes WHERE post_id='.$tempId;
+	    $countResult = mysqli_query($conn, $countQuery);
+            $numWebsiteVotes += $countResult->num_rows;
+	}
+
+	$result = mysqli_query($conn, $articleQuery);
+	while($row = mysqli_fetch_array($result)){
+	    $tempId = $row['post_id'];
+            $countQuery = 'SELECT * FROM project_upvotes WHERE post_id='.$tempId;
+	    $countResult = mysqli_query($conn, $countQuery);
+            $numArticleVotes += $countResult->num_rows;
+	}
+
+
+	$result = mysqli_query($conn, $bookQuery);
+	while($row = mysqli_fetch_array($result)){
+	    $tempId = $row['post_id'];
+            $countQuery = 'SELECT * FROM project_upvotes WHERE post_id='.$tempId;
+	    $countResult = mysqli_query($conn, $countQuery);
+            $numBookVotes += $countResult->num_rows;
+	}
+
+	$result = mysqli_query($conn, $videoQuery);
+	while($row = mysqli_fetch_array($result)){
+	    $tempId = $row['post_id'];
+            $countQuery = 'SELECT * FROM project_upvotes WHERE post_id='.$tempId;
+	    $countResult = mysqli_query($conn, $countQuery);
+            $numVideoVotes += $countResult->num_rows;
+	}
+
+
+
+?>
+<script>
+var randomScalingFactor = function() {
+        return Math.round(Math.random() * 100);
+};
+var configOne = {
+        type: 'pie',
+        data: {
+                datasets: [{
+                    data: [
+                <?PHP
+                echo $numWebsites.",".$numBooks.",".$numArticles.",".$numVideos;
+                ?>
+                ],
+                backgroundColor: [
+                    window.chartColors.red,
+                    window.chartColors.green,
+		    window.chartColors.blue,
+		    window.chartColors.yellow
+                                    ],
+                label: 'Dataset 1'
+                }],
+                labels: [
+                    "Website",
+                    "Book",
+	    	    "Article",
+	    	    "Video"
+                ]
+        },
+        options: {
+            responsive: true
+        }
+    };
+var configTwo = {
+        type: 'pie',
+        data: {
+                datasets: [{
+                    data: [
+                <?PHP
+                echo $numWebsiteVotes.",".$numBookVotes.",".$numArticleVotes.",".$numVideoVotes;
+                ?>
+                ],
+                backgroundColor: [
+                    window.chartColors.red,
+                    window.chartColors.green,
+		    window.chartColors.blue,
+		    window.chartColors.yellow
+                                    ],
+                label: 'Dataset 1'
+                }],
+                labels: [
+                    "Website",
+                    "Book",
+	    	    "Article",
+	    	    "Video"
+                ]
+        },
+        options: {
+            responsive: true
+        }
+    };
+window.onload = function() {
+        var ctx = document.getElementById("chart-area-1").getContext("2d");
+        window.myPie = new Chart(ctx, configOne);
+
+	var ctx = document.getElementById("chart-area-2").getContext("2d");
+        window.myPie = new Chart(ctx, configTwo);
+    };
+
+    document.getElementById('randomizeData').addEventListener('click', function() {
+        config.data.datasets.forEach(function(dataset) {
+            dataset.data = dataset.data.map(function() {
+                return randomScalingFactor();
+            });
+        });
+
+        window.myPie.update();
+    });
+ var colorNames = Object.keys(window.chartColors);
+    document.getElementById('addDataset').addEventListener('click', function() {
+        var newDataset = {
+            backgroundColor: [],
+            data: [],
+            label: 'New dataset ' + config.data.datasets.length,
+        };
+
+        for (var index = 0; index < config.data.labels.length; ++index) {
+            newDataset.data.push(randomScalingFactor());
+
+            var colorName = colorNames[index % colorNames.length];;
+            var newColor = window.chartColors[colorName];
+            newDataset.backgroundColor.push(newColor);
+        }
+
+        config.data.datasets.push(newDataset);
+        window.myPie.update();
+    });
+
+    document.getElementById('removeDataset').addEventListener('click', function() {
+        config.data.datasets.splice(0, 1);
+        window.myPie.update();
+    });
+</script>
+
 
 </div>
 
