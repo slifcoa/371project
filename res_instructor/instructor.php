556ef216bbac296dca730dfa1c2e9231902eaa1e
norm394 <cunningn@mail.gvsu.edu>
2018-04-14 16:02:28 -0400

    updated

--- res_instructor/instructor.php
+++ res_instructor/instructor.php
@@ -6,13 +6,15 @@
     // Authenticate that the user came from BlackBoard.
     require_once($path_auth);
 
+    include_once $path_sql_queries;
 
-    $conn = mysqli_connect("34.224.83.184","student33","phppass33","student33");
-    if(isset($_POST['submit_btn'])) {
-	$title = mysqli_real_escape_string($conn, $_POST['title']);
-	$link =  mysqli_real_escape_string($conn, $_POST['link']);
-	$type =  mysqli_real_escape_string($conn, $_POST['type']);
-	$description = mysqli_real_escape_string($conn, $_POST['description']);
+
+    //$conn = mysqli_connect("34.224.83.184","student33","phppass33","student33");
+    //if(isset($_POST['submit_btn'])) {
+	$title = mysqli_real_escape_string($db_connection, $_POST['title']);
+	$link =  mysqli_real_escape_string($db_connection, $_POST['link']);
+	$type =  mysqli_real_escape_string($db_connection, $_POST['type']);
+	$description = mysqli_real_escape_string($db_connection, $_POST['description']);
 	/* $instructor */
 	$course_id = $_SESSION['course_id'];
         $user_name = $_SESSION['user_name'];
@@ -22,41 +24,47 @@
 $query = <<<SQL
 
     INSERT INTO project_posts (title, description, link, course_id, posted_by, type)
-    VALUES ('{$title}', '{$description}', '{$link}', '{$course_id}','Chris', '{$type}')
+    VALUES ('{$title}', '{$description}', '{$link}', '{$course_id}','{$user_name}', '{$type}')
 
 SQL;
 
-	$result = mysqli_query($conn, $query); 
+	$result = mysqli_query($db_connection, $query); 
 	if($result==true){
 	    unset($_POST['submit_btn']);
 	}
-    }
+    //}
 ?>
 
 
 <!DOCTYPE html>
 <html>
 <head>
-    <script src="https://cis371a.hopto.org:9040/demo/Chart.bundle.js"></script>
-    <script src="https://cis371a.hopto.org:9040/demo/utils.js"></script>
+    <!--<script src="https://cis371a.hopto.org:9040/demo/Chart.bundle.js"></script>
+    <script src="https://cis371a.hopto.org:9040/demo/utils.js"></script>-->
+
+    <script src="https://nlc3.hopto.org:9033/371project/res_instructor/Chart.bundle.js"></script>
+    <script src="https://nlc3.hopto.org:9033/371project/res_instructor/utils.js"></script>
     <link href="https://code.jquery.com/ui/jquery-ui-git.css" rel="stylesheet" type="text/css" />
     <script src="https://code.jquery.com/jquery-git.js"></script>
     <script src="https://code.jquery.com/ui/jquery-ui-git.js"></script>
-    <link rel="stylesheet" href="../res_student/css/css_main.css">
+   
     <script type="text/javascript" src="../res_student/js/js_config.js"></script>
     <script type="text/javascript" src="../res_student/js/js_util.js"></script>
     <link rel="stylesheet" type="text/css" href="./css/instructor_styles.css">
+    <link rel="stylesheet" type="text/css" href="../res_student/css/css_main.css">
+    <link rel="stylesheet" type="text/css" href="../res_student/css/css_student.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
 </head>
 
 
-<body>
+<body class="w3-light-grey">
 <div class="page">
 <br/><br/>
 
 <!-- HEADER -->
 <div class="header">
-<h1>$teamName</h1>
-<h2>Share a Resource with <?php echo "courseName" ?></h2>
+
+<h1>Share a Resource with <?php echo $_SESSION['course_name']; ?></h1>
 </div>
 <br /><br />
 
@@ -110,30 +118,6 @@ SQL;
 </div>
 <!-- END FORM -->
 
-<!-- SHOW POSTS -->
-<div id="posts-container">
-<br/>
-<h3>Posts</h3>
-    <?PHP
-	$postQuery = "SELECT * FROM project_posts ORDER BY post_id DESC";
-	$results = mysqli_query($conn, $postQuery);
-
-    if ($results == false){
-        echo "Bad Dog";
-    }
-
-	while($row = mysqli_fetch_array($results)){
-	    echo "<div class=post>";
-		echo $row['title'];
-		echo $row['link'];
-		echo $row['description'];
-	    echo "</div>";
-}	    echo "<br/><br/>";
-?>     
-<br/><br/>
-</div>
-<!-- END POSTS -->
-
 <!-- STATS BUTTON -->
 <div class="stats_btn">
     <button id="btn" type="button">Get Resource Statistics for this Course</button>
@@ -159,16 +143,16 @@ SQL;
 	$articleQuery = "SELECT * FROM project_posts WHERE type='article'";
 	$videoQuery = "SELECT * FROM project_posts WHERE type='video'";
 	
-	$result = mysqli_query($conn, $websiteQuery);
+	$result = mysqli_query($db_connection, $websiteQuery);
 	$numWebsites = $result->num_rows;
 	
-	$result= mysqli_query($conn, $bookQuery);
+	$result= mysqli_query($db_connection, $bookQuery);
 	$numBooks = $result->num_rows;
 	 
-	$result = mysqli_query($conn, $articleQuery);
+	$result = mysqli_query($db_connection, $articleQuery);
 	$numArticles = $result->num_rows;
 	
-	$result = mysqli_query($conn, $videoQuery);
+	$result = mysqli_query($db_connection, $videoQuery);
 	$numVideos = $result->num_rows;
 
 ?>
@@ -261,36 +245,36 @@ window.onload = function() {
 	$numVideoVotes = 0;
 	$tempId;
 
-	$result = mysqli_query($conn, $websiteQuery);
+	$result = mysqli_query($db_connection, $websiteQuery);
 	while($row = mysqli_fetch_array($result)){
 	    $tempId = $row['post_id'];
             $countQuery = 'SELECT * FROM project_upvotes WHERE post_id='.$tempId;
-	    $countResult = mysqli_query($conn, $countQuery);
+	    $countResult = mysqli_query($db_connection, $countQuery);
             $numWebsiteVotes += $countResult->num_rows;
 	}
 
-	$result = mysqli_query($conn, $articleQuery);
+	$result = mysqli_query($db_connection, $articleQuery);
 	while($row = mysqli_fetch_array($result)){
 	    $tempId = $row['post_id'];
             $countQuery = 'SELECT * FROM project_upvotes WHERE post_id='.$tempId;
-	    $countResult = mysqli_query($conn, $countQuery);
+	    $countResult = mysqli_query($db_connection, $countQuery);
             $numArticleVotes += $countResult->num_rows;
 	}
 
 
-	$result = mysqli_query($conn, $bookQuery);
+	$result = mysqli_query($db_connection, $bookQuery);
 	while($row = mysqli_fetch_array($result)){
 	    $tempId = $row['post_id'];
             $countQuery = 'SELECT * FROM project_upvotes WHERE post_id='.$tempId;
-	    $countResult = mysqli_query($conn, $countQuery);
+	    $countResult = mysqli_query($db_connection, $countQuery);
             $numBookVotes += $countResult->num_rows;
 	}
 
-	$result = mysqli_query($conn, $videoQuery);
+	$result = mysqli_query($db_connection, $videoQuery);
 	while($row = mysqli_fetch_array($result)){
 	    $tempId = $row['post_id'];
             $countQuery = 'SELECT * FROM project_upvotes WHERE post_id='.$tempId;
-	    $countResult = mysqli_query($conn, $countQuery);
+	    $countResult = mysqli_query($db_connection, $countQuery);
             $numVideoVotes += $countResult->num_rows;
 	}
 
@@ -430,6 +414,117 @@ window.onload = function() {
 <br/><br/>
 </div>
 
+<!-- SHOW POSTS -->
+
+<br/>
+<h3>Posts</h3>
+<div class="w3-content width_max_75 w3-light-grey">
+
+<div id=entry_grid class="w3-col l8 s12">
+    <?PHP
+
+    $postQuery = "SELECT * FROM project_posts ORDER BY post_id DESC";
+    $results = mysqli_query($db_connection, $postQuery);
+
+    if ($results == false){
+        echo "No posts to view";
+    }
+
+    while($row = mysqli_fetch_array($results)){
+
+        $post_id = $row['post_id'];
+        $title = $row['title'];
+        $link  = $row['link'];
+        $description = $row['description'];
+        $num_upvotes  = sql_select_post_vote_total($post_id);
+        $num_comments = sql_select_post_comment_total($post_id);
+       
+
+echo <<<HTML
+
+            <div    id="{$post_id}" 
+                    class="w3-card-4 w3-margin w3-white type{$type}">
+                {$image}
+                <div class="w3-container">
+                    <h3>
+                        <b>
+                           {$row['title']}
+                        </b>
+                    </h3>
+                    <h5>
+                        <a href="{$link}">
+                            {$link}
+                        </a>
+                    </h5>
+                </div>
+                <div class="w3-container">
+                    <p>
+                        {$description}
+                    </p>
+                    <div class="w3-row">   
+                        <div class='w3-col m8 s12'>
+                            <p>
+                                
+                                <button id="btn{$post_id}" 
+                                        onClick="js_view_comments({$post_id})" 
+                                       class="w3-button w3-padding-large w3-white w3-border">
+                                    <b> 
+                                        VIEW COMMENTS »
+                                    </b>
+                                </button>
+                            </p>
+                        </div>
+                        <div class='w3-col m4 w3-hide-small'>
+                            <p>
+                                <span class="w3-padding-large w3-right">
+                                    <b> 
+                                        Upvotes 
+                                    </b> 
+                                    &ensp;
+                                    <span   id="upvotes{$post_id}" 
+                                            class="w3-tag">
+                                        {$num_upvotes}
+                                    </span>
+                                </span>
+                            </p>                         
+                            <p>
+                                <span class="w3-padding-large w3-right">
+                                    <b>
+                                        Comments  
+                                    </b> 
+                                    &ensp;
+                                    <span   id="comments{$post_id}" 
+                                            class="w3-tag">
+                                        {$num_comments}
+                                    </span>
+                                </span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+           <!-- <hr> -->
+HTML;
+
+}
+/*
+        echo "<div class=post>";
+        echo $row['title'];
+        echo $row['link'];
+        echo $row['description'];
+        echo "</div>";
+}*/     echo "<br/><br/>";
+?> 
+</div> 
+</div>   
+<br/><br/>
+<!-- END POSTS -->
+
+
+
+
+<script type="text/javascript" src="../res_student/js/js_filter_posts.js"></script>
+<script type="text/javascript" src="../res_student/js/js_sort_posts.js"></script>
 </body>
 </html>
 
